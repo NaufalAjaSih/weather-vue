74322fd683c4da16e3b60af35c673c457bd7f340
NaufalAjaSih <masanaufal@gmail.com>
2023-05-09 14:42:34 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col min-h-screen bg-gray-700 font-Roboto">
+    <SiteNavigation />
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import SiteNavigation from "./components/SiteNavigation.vue";
+</script>
+
+<style lang="scss" scoped></style>
